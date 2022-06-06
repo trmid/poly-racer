0d4e7d6925dd7c5bcc05f1cf92e691c83b171483
midpoint68 <trmidpoint@gmail.com>
2022-06-06 16:07:35 -0400

    remove track recommendations

--- src/routes/RandomRace.svelte
+++ src/routes/RandomRace.svelte
@@ -1,5 +1,4 @@
 <script type="ts">
-  import { link } from "svelte-spa-router";
   import { push } from "svelte-spa-router";
   import { generate32bitSeed } from "../ts/random";
   push(`/race/${generate32bitSeed()}`);
@@ -7,10 +6,3 @@
 <p>
   Redirecting to random race...
 </p>
-
-<h3>Some Nice Tracks:</h3>
-<ol>
-  <li><a href="/race/3ee46eb0" use:link>3ee46eb0</a></li>
-  <li><a href="/race/c19b919d" use:link>c19b919d</a></li>
-  <li><a href="/race/a6948b93" use:link>a6948b93</a></li>
-</ol>
